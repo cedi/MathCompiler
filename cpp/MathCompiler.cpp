35aff4442b6219f12f98bf92deeac321f655b142
Cedric Kienzler <c.kienzler@gmx.net>
2015-10-12 19:37:08 +0200

    Update Comment

--- cpp/MathCompiler.cpp
+++ cpp/MathCompiler.cpp
@@ -95,10 +95,7 @@ void Compiler::compileSingleOperation(CalculationDirectionEnum direction, Mathem
 
 			while(expression->find(op) > -1)
 			{
-<<<<<<< HEAD
-=======
-// todo/ck/no direction enum and string needed. Operator contains direction and string. 
->>>>>>> master
+				// todo/ck/no direction enum and string needed. Operator contains direction and string.
 				pair<int, int> ret = expression->getOperatorIdxFromStr(direction, op);
 				int index = ret.first;
 				int length = ret.second;
